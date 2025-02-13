dd2f9bc5240f415bfafc14dc680bbde40c65be69
lucahliau <lucaliautaud1@gmail.com>
2025-02-13 18:02:14 -0500

    bb

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
 
 // Middleware
 app.use(cors());
