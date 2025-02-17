b02da2170b8399828dc214a9f59e4c483c260af0
lucahliau <lucaliautaud1@gmail.com>
2025-02-17 18:09:46 -0500

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,29 +1,13 @@
 // models/Post.js
-/*
-const mongoose = require('mongoose');
-
-const PostSchema = new mongoose.Schema({
-  imageUrl: { type: String, required: true },
-  title: { type: String, required: true },
-  description: {type: String, default: ""},
-  uploader: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('Post', PostSchema);
- */
-// models/Post.js
 
 const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
-  imageUrl: { type: String, required: true },
-  linkUrl: { type: String, required: true },
-  title: { type: String, required: true },
-  description: { type: String, required: true },
+  "image_url:": { type: String, required: true },
+  "title:": { type: String, required: true },
   uploader: { type: String, required: true },
-  price: { type: Number, required: true },
+  "price:": { type: Number, required: true },
   priceRange: { type: String },
-  createdAt: { type: Date, default: Date.now },
   "product_description:": { type: String }
 });
 
