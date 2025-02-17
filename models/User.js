3fd54d5432b8b7675a7edad66a44e8ef95c9047d
lucahliau <lucaliautaud1@gmail.com>
2025-02-16 23:20:56 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  userType: { type: String, enum: ['upload', 'mobile'], required: true },
+  //userType: { type: String, enum: ['upload', 'mobile'], required: true },
   likedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
   dislikedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
   likedClusters: { type: Array, default: [] },
