4f5ee8eae44ca2c5635cb3018ee41e63693cc013
lucahliau <lucaliautaud1@gmail.com>
2025-02-18 00:18:07 -0500

    Create CommunityPost.js

---
+++ models/CommunityPost.js
@@ -0,0 +1,18 @@
+// models/communityPosts.js
+const mongoose = require('mongoose');
+
+// models/communityPosts.js
+const UserSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  likedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+  dislikedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+  likedClusters: { type: Array, default: [] },
+  dislikedClusters: { type: Array, default: [] }
+});
+
+
+module.exports = mongoose.model('User', UserSchema);
