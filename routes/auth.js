8ac846878f5c4d0195ba38f26f24d2c6bf52a43c
lucahliau <lucaliautaud1@gmail.com>
2025-02-19 13:53:03 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -271,7 +271,7 @@ const authMiddleware = require('../middleware/auth');
 router.get('/profile', authMiddleware, async (req, res) => {
   try {
     // Find the user by the ID from the token and populate liked and disliked posts
-    const user = await User.findById(req.user.id).populate('likedPosts dislikedPosts');
+    const user = await User.findById(req.user.id).populate('likedPosts dislikedPosts favouritePosts');
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
